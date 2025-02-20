2d20f8f26f72d561229c6a2c0ab2a056b632d797
Aravindhan-Senthilkumar <aravindhan.cofe@gmail.com>
2025-02-20 16:58:42 +0530

    "First Changes"

---
+++ src/screens/BarcodeGeneration.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BarcodeGeneration = () => {
+  return (
+    <View>
+      <Text>BarcodeGeneration</Text>
+    </View>
+  )
+}
+
+export default BarcodeGeneration
+
+const styles = StyleSheet.create({})
